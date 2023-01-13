f76fbe133ab99419e1c37c6d94b625b02ec7f738
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-01-13 21:36:56 +0900

    ローディングコンポーネントとデフォルトレイアウト設定

---
+++ components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="whole-wrapper">
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.whole-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: $bg-yellow;
+}
+</style>
