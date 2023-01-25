570d6dffcb16f3c25cb4bbd76a7eb8a50e491e14
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-01-26 00:27:09 +0900

    削除

---
+++ components/WorkCardList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="work-card-list">
+    <WorkCard />
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.work-card-list {
+  display: grid;
+  padding-left: 16px;
+  padding-right: 16px;
+  margin-top: 12px;
+  grid-gap: 12px;
+  grid-template-columns: repeat(auto-fill,minmax(400px,1fr));
+  width: 900px;
+}
+</style>
