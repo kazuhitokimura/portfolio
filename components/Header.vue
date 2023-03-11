c6446f3235a93b61c03db9c6a9b3d1ca2ec17cf5
kazuhitokimura <kimura.k@partnersuccess.co.jp>
2023-03-11 11:52:42 +0900

    headerのスタイル

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <header>
-    <ContactBtn />
+    <div class="contents">
+      <ContactBtn />
+    </div>
   </header>
 </template>
 
@@ -13,23 +15,33 @@ import ContactBtn from './ContactBtn.vue';
 header {
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: center;
   align-items: center;
   height: 72px;
   width: 100%;
-  padding-left: 40px;
-  padding-right: 40px;
   background-color: $surface-white;
   box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.04);
   position: fixed;
   z-index: 9999;
+
+  .contents {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    height: 72px;
+    width: 1280px;
+    margin-right: 16px;
+    margin-left: 16px;
+  }
 }
 
 @media (max-width: 480px) {
 
   header {
-    padding-left: 16px;
-    padding-right: 16px;
+    .contents{
+      width: 100%;
+    }
   }
 }
 </style>
